bdbd7768688e826a69d0c9e5db703e74d64f31cc
Yacine Grouci <wwwgrouciyacine@gmail.com>
2023-06-10 00:55:22 +0200

    Update tokenVer.js

--- tokenVer.js
+++ tokenVer.js
@@ -1,8 +1,8 @@
 import jwt from 'jsonwebtoken'
 
 export const verify=(req,res,next)=>{
-//const token=req.cookies.access_token;
-const token= req.headers.access_token
+const token=req.cookies.access_token;
+//const token= req.headers.access_token
 if(!token) return res.status(500).json('error token not exist')
 jwt.verify(token,process.env.JWT,(err,user)=>{
     if(err) return res.status(500).json('err user not exist')
