e9ba7775636119567273f1a6efa5ec584d8d7a41
Yacine Grouci <wwwgrouciyacine@gmail.com>
2023-06-09 15:10:28 +0200

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -33,7 +33,7 @@ export const login=async(req,res,next)=>{
     }else{
         const token=jwt.sign({id:user._id},process.env.JWT);
         const {password,...other}=user._doc;
-        res.cookie('access_token',token,{ secure: true});
+        res.cookie('access_token',token,{ secure: true,HttpOnly:true});
         res.status(200).json(other);
     }
     }else{
