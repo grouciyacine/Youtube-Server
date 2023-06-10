263a6f15526129bbd30467c27919a80c2302c9eb
Yacine Grouci <wwwgrouciyacine@gmail.com>
2023-06-10 02:55:32 +0200

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -33,10 +33,7 @@ export const login=async(req,res,next)=>{
     }else{
         const token=jwt.sign({id:user._id},process.env.JWT);
         const {password,...other}=user._doc;
-        res.cookie('access_token',token,{  domain: '.vercel.app',
-  path: '/',
-  secure: true,
-  sameSite: 'None',});
+    res.cookie('access_token', token, { secure: true, httpOnly: true });
         res.status(200).json(other);
     }
     }else{
